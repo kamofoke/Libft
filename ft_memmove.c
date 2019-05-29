476deb678089d2644ffa7f236b7faf53259c6713
Katleho MOFOKENG <kamofoke@c4r6s11.wethinkcode.co.za>
2019-05-29 14:20:52 +0200

    move

---
+++ ft_memmove.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kamofoke <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/29 13:57:33 by kamofoke          #+#    #+#             */
+/*   Updated: 2019/05/29 14:19:15 by kamofoke         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void 	*ft_memmove(void *dest, const void *src, size_t len);
+{
+	char *des;
+	const char *sc;
+
+	des = dest;
+	sc = src;
+	if (sc == des)
+		return (dest);
+	if (sc < des)
+	{
+		sc = sc + len - 1;
+		des = des + len - 1;
+		while(n--)
+			*des-- = *sc--;
+	}
+	else 
+		while (n--)
+			*des++ = *sc++;
+	return(dest);
+}
