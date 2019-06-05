7207b46c94a3beaac0afd95a99e04fb39f18cb01
Katleho MOFOKENG <kamofoke@c5r6s9.wethinkcode.co.za>
2019-06-05 17:24:19 +0200

    update

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: kamofoke <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/31 10:26:12 by kamofoke          #+#    #+#             */
-/*   Updated: 2019/06/05 17:17:55 by kamofoke         ###   ########.fr       */
+/*   Updated: 2019/06/05 17:24:00 by kamofoke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,6 @@ void	*ft_memset(void *b, int c, size_t len);
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void	*ft_memchr(const void *str, int c, size_t n);
 int		ft_putchar(char c);
-void	ft_putnbr(int nb);
 void	ft_putstr(char *str);
 char	*ft_strcat(char *dest, const char *src);
 char	*ft_strchr(const char *str, int i);
