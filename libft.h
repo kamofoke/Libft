ea21952dcbda100492c0281f1905d9b74a75586e
Katleho MOFOKENG <kamofoke@c5r6s9.wethinkcode.co.za>
2019-06-05 17:05:12 +0200

    update

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: kamofoke <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/31 10:26:12 by kamofoke          #+#    #+#             */
-/*   Updated: 2019/06/05 16:44:25 by kamofoke         ###   ########.fr       */
+/*   Updated: 2019/06/05 17:04:53 by kamofoke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,17 +34,12 @@ void	ft_putnbr(int nb);
 void	ft_putstr(char *str);
 char	*ft_strcat(char *dest, const char *src);
 char	*ft_strchr(const char *str, int i);
-int		ft_strcmp(char *str1,const char *str2);
-char	*ft_strdup(const char *s1);
+int		ft_strcmp(const char *str1,const char *str2);
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
 int		ft_strlen(char *str);
-int		ft_strncmp(const char *str1, const char *str2, size_t n);
 char	*ft_strcpy(char *dest, char *src);
 char	*ft_strncpy(char *str, char *dest, size_t n);
-char	*ft_strnstr(const char *str1, const char str2, size_t len);
 char	ft_strncat(char *s1, const char *s2, size_t n);
-char	*ft_strrchr(const char *s, int c);
-int		*ft_strstr(char *str1, const char *str2);
 int		ft_tolower(char c);
 int		ft_toupper(char c);
 
