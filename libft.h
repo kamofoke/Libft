9a84d08f99678cb3d951faa230dd3e9a23a86983
Katleho MOFOKENG <kamofoke@c5r6s9.wethinkcode.co.za>
2019-06-06 14:04:51 +0200

    update

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: kamofoke <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/31 10:26:12 by kamofoke          #+#    #+#             */
-/*   Updated: 2019/06/06 13:52:40 by kamofoke         ###   ########.fr       */
+/*   Updated: 2019/06/06 14:03:06 by kamofoke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,12 +38,12 @@ void				*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void				*ft_memchr(const void *str, int c, size_t n);
 int					ft_putchar(char c);
 void				ft_putstr(char *str);
-char				*ft_strcat(char *dest, const char *src);i
-char				*ft_strdup(const char *s1)
+char				*ft_strcat(char *dest, const char *src);
+char				*ft_strdup(const char *s1);
 char				*ft_strchr(const char *str, int i);
 char				*ft_strncpy(char *dst, const char *src, size_t len);
 int					ft_strcmp(const char *str1, const char *str2);
-int					ft_strncmp(const char *s1, const char *s2. size_t n);
+int					ft_strncmp(const char *s1, const char *s2, size_t n);
 size_t				ft_strlcat(char *dest, const char *src, size_t size);
 int					ft_strlen(char *str);
 char				*ft_strcpy(char *dest, char *src);
